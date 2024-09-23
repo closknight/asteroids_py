8af0063a55e71c4d19f0e2fb39c6de0be21ec260
Carlos Caballero <carlos8caballero@gmail.com>
2024-09-23 10:24:02 -0700

    Add asteroid spliting

--- main.py
+++ main.py
@@ -41,6 +41,11 @@ def main():
             if player.collides_with(asteroid):
                 print("Game over!")
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
         
         screen.fill("black")
 
